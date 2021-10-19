3ee29ebae28e2f0c08dd969a4653e9f51cbb6dfc
DURISHETTY BHAVANI <DURISHETTY BHAVANI@DESKTOP-H1O0LU7>
2021-10-19 12:57:52 +0530

    All things has been completed.

---
+++ EmployeeProject/src/com/training/User.java
@@ -0,0 +1,34 @@
+package com.training;
+
+public class User {
+	private int empId;
+	private String empName;
+	private String empEmail;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpEmail() {
+		return empEmail;
+	}
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "User [empId=" + empId + ", empName=" + empName + ", empEmail=" + empEmail + "]";
+	}
+	
+}
